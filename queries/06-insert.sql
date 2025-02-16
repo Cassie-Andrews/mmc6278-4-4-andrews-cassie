3bd2ccdbe17e30d71671b13124f9b20a57332430
Cassie Andrews <candrews1@ufl.edu>
2025-02-15 23:20:05 -0500

    Query 06

--- queries/06-insert.sql
+++ queries/06-insert.sql
@@ -1,2 +1,3 @@
 -- Add Track #13 titled "Endless, Nameless" to Nirvana's Nevermind album
 -- Nirvana's id is 6, Nevermind's id is 9
+INSERT INTO songs (name, track_number, artist_id, album_id) values ("Endless, Nameless", "13", "6", "9");
